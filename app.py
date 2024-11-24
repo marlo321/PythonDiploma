6bdf6d5ca92785a150c07e6c93bb1013639801b2
Your Name <marko.goliak@gmail.com>
2024-11-24 18:10:46 +0200

    updated app

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from typing import Optional
-from ensemble import ensemble_predict_end_date as ensemble
+from ensemble import ensemble_predict_end_date
 
 
 app = FastAPI()
@@ -21,13 +21,13 @@ async def ensemble_predict_end_date(
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-if __name__ == "__main__":
-    current_stock = 10
-    bmw_model = "BMW 3 Series"
-    car_part = "Brake Pads"
-
-    try:
-        prediction = ensemble(current_stock, bmw_model, car_part)
-        print(prediction)
-    except ValueError as e:
-        print(f"Error: {e}")
+# if __name__ == "__main__":
+    # current_stock = 10
+    # bmw_model = "BMW 3 Series"
+    # car_part = "Brake Pads"
+    #
+    # try:
+    #     prediction = ensemble(current_stock, bmw_model, car_part)
+    #     print(prediction)
+    # except ValueError as e:
+    #     print(f"Error: {e}")
